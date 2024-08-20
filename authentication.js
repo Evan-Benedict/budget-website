9e3076d71b7777ccd497aa95f84439b3efc66cfa
Evan-Benedict <90004851+Evan-Benedict@users.noreply.github.com>
2024-08-20 15:20:51 -0500

    fixed broken submission

--- authentication.js
+++ authentication.js
@@ -6,11 +6,12 @@ const form = document.getElementById('authentication-form');
     const correctCode = '3636';
 
     form.addEventListener('submit', function(event) {
-        event.preventDefault();
+        event.preventDefault(); // Prevent the form from submitting in the traditional way
         const enteredCode = codeField.value;
 
         // Compare the entered code with the correct code
         if (enteredCode === correctCode) {
+
             window.location.href = "./AppContent/income.html";
         } else {
             errorMsg.style.opacity = '1'; // Show the error message
